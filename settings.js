a378c16c8bbb1ec13f5aaa7b28dededdecd4425b
aryaappu920 <97217306+aryaappu920@users.noreply.github.com>
2022-05-23 10:42:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,16 +19,16 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
-global.ig = 'https://github.com/nexusNw'
+global.owner = ['918075116840']
+global.premium = ['918075116840']
+global.ownernomer = '918075116840'
+global.ownername = 'ᴀᴅɪᴛʜ'
+global.botname = 'ᴍʀ_ʙᴏᴛ_ᴍᴅ'
+global.footer = '©ᴍʀ ʙᴏᴛ.'
+global.ig = 'https://github.com'
 global.region = 'India, South India, Kerala'
-global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
+global.sc = 'https://github.com'
+global.myweb = 'https://youtube.com'
 global.packname = 'Gojo-Satoru'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
